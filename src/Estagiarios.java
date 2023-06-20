6c7c7b8d54ec06707ea8d16f137ec8820b98daaf
thaisbreder <thais.breder@zup.com.br>
2023-06-19 21:16:57 -0300

    :sparkles: feat: funcionalidade do professor adicionar novos estagiários supervisionados

--- src/Estagiarios.java
+++ src/Estagiarios.java
@@ -1,10 +1,15 @@
 public class Estagiarios extends Professores {
+
+    public String nivelGraduacao;
+    public String disciplina;
     public Estagiarios(String nome, int cpf, int numRegistro, int orgaoLotacao, double salario, String nivelGraduacao, String disciplina) {
-        super(nome, cpf, numRegistro, orgaoLotacao, salario, nivelGraduacao, disciplina, 0, 0);
+        super(nome, cpf, numRegistro, orgaoLotacao, salario);
+        this.nivelGraduacao = nivelGraduacao;
+        this.disciplina = disciplina;
     }
 
     public void exibirInformacoes(int cargo) {
-         System.out.println("\nInformações do Estagiário:\n");
+        System.out.println("\nInformações do Estagiário:\n");
         super.exibirInformacoes(cargo);
         System.out.println("Nível de Graduação: " + getNivelGraduacao());
         System.out.println("Bolsa antes do aumento: " + getSalario());
--- src/Professores.java
+++ src/Professores.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Professores extends Funcionarios {
 
@@ -58,14 +59,13 @@ public class Professores extends Funcionarios {
         return estagiariosSupervisionados;
     }
 
- 
-
     public void exibirInformacoes(int cargo) {
-               System.out.println("\nInformações do Professor:\n");
+        Scanner entradaDoUsuario = new Scanner(System.in);
+        System.out.println("\nInformações do Professor:\n");
 
         super.exibirInformacoes(cargo);
-        System.out.println("Nível de Graduação: " +  getNivelGraduacao());
-        System.out.println("Disciplina: " +  getDisciplina());
+        System.out.println("Nível de Graduação: " + getNivelGraduacao());
+        System.out.println("Disciplina: " + getDisciplina());
         System.out.println("Quantidade de Alunos: " + getQntAlunos());
         System.out.println("Quantidade de Turmas: " + getQntTurmas());
         System.out.print("Estagiarios Supervisionados: ");
@@ -74,6 +74,37 @@ public class Professores extends Funcionarios {
         }
         System.out.println();
 
+        System.out.println("\nGostaria de adicionar um novo estágiario para ser supervisionado? \n1 - SIM \n2 - NÃO");
+        String addEstagiario = entradaDoUsuario.next();
+        if (addEstagiario.equals("1") && getEstagiariosSupervisionados().size() <= 1) {
+            System.out.print("Digite o primeiro nome do novo estagiario(a): ");
+            String nomeEstagiario = entradaDoUsuario.next();
+            System.out.print("Digite o CPF do novo estagiario(a): ");
+            int cpfEstagiario = entradaDoUsuario.nextInt();
+            System.out.print("Digite o número de registro do novo estagiario(a): ");
+            int numRegistroEstagiario = entradaDoUsuario.nextInt();
+            System.out.print("Digite o orgão de lotação do novo estagiario(a): ");
+            int orgaoLotacaoEstagiario = entradaDoUsuario.nextInt();
+            System.out.print("Digite o salário do novo estagiario(a): ");
+            double salarioEstagiario = entradaDoUsuario.nextDouble();
+            System.out.print("Digite o nível ou curso de graduação do novo estagiario(a): ");
+            String nivelGraduacaoEstagiario = entradaDoUsuario.next();
+            System.out.print("Digite a disciplina que o novo estagiario(a) será responsável: ");
+            String disciplinaEstagiario = entradaDoUsuario.next();
+
+            Estagiarios novoEstagiario = new Estagiarios(nomeEstagiario, cpfEstagiario, numRegistroEstagiario, orgaoLotacaoEstagiario, salarioEstagiario, nivelGraduacaoEstagiario, disciplinaEstagiario);
+            adicionaEstagiarios(novoEstagiario);
+
+            System.out.println("Estagiário(a) adicionado com sucesso!");
+
+            System.out.print("Estagiários Supervisionados: ");
+            for (Estagiarios estagiario : getEstagiariosSupervisionados()) {
+                System.out.print(estagiario.getNome() + " | ");
+            }
+            System.out.println();
+        } else {
+            System.out.println("ERRO: Limite máximo de estagiários supervisionados atingido!");
+        }
 
     }
 
